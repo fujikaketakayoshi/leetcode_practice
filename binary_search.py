ba0bdc582413388708e075a9bf46acc2fa9db674
fjkk <fujikake.takayoshi@gmail.com>
2025-09-01 23:28:07 +0900

    printしてmidとl,rを追跡、numsを大きくしてさらに追跡

--- binary_search.py
+++ binary_search.py
@@ -12,7 +12,7 @@ def binary_search(nums, target):
             r = mid - 1   # 左半分へ
     return -1  # 見つからなかった場合
 
-nums = [1, 2, 3, 5, 6, 7, 8, 9, 12, 15, 18]
-target = 20
+nums = [1, 2, 3, 5, 6, 7, 8, 9, 12, 15, 18, 20, 25, 30, 35, 40, 50, 60, 70, 80, 90]
+target = 12
 result = binary_search(nums, target)
 print(result)
