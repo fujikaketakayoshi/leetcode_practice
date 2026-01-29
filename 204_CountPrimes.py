89847faa01806326f83e33f098c4285672a12885
FUJIKAKE Takayoshi <fujikake.takayoshi@gmail.com>
2026-01-29 16:45:43 +0900

    素数をエストラテネスの櫛で求める、iからnの平方根+1まで、jをi**からn+1までiスキップで回す

---
+++ 204_CountPrimes.py
@@ -0,0 +1,24 @@
+class Solution:
+    def countPrimes(self, n: int) -> int:
+        if n < 2:
+            return 0
+        
+        is_prime = [True] * (n + 1)
+        is_prime[0] = is_prime[1] = False
+
+        for i in range(2, int(n**0.5) + 1): 
+            if is_prime[i]:
+                for j in range(i * i, n + 1, i):
+                    print(i, j)
+                    is_prime[j] = False
+
+        return len([i for i in range(2, n) if is_prime[i]])
+
+solution = Solution()
+print(solution.countPrimes(10))  # 出力: 4
+print(solution.countPrimes(0))  # 出力: 0
+print(solution.countPrimes(1))  # 出力: 0
+print(solution.countPrimes(2))  # 出力: 0
+print(solution.countPrimes(3))  # 出力: 1
+print(solution.countPrimes(100))  # 出力: 25
+print(solution.countPrimes(150))  # 出力: 35
