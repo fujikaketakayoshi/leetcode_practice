f4dcc92dc40fb75592e73705798b599682cfc5b0
FUJIKAKE Takayoshi <fujikake.takayoshi@gmail.com>
2026-06-25 17:25:03 +0900

    1162261467の代わりに3**19とした

--- 326_PowerofThree_chatgpt.py
+++ 326_PowerofThree_chatgpt.py
@@ -1,6 +1,7 @@
 class Solution:
     def isPowerOfThree(self, n: int) -> bool:
-        return n > 0 and 1162261467 % n == 0
+        # return n > 0 and 1162261467 % n == 0
+        return n > 0 and 3 ** 19 % n == 0
 
 s = Solution()
 print(s.isPowerOfThree(27))
