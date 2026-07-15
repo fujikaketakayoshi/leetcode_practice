898d0863547516a3cd8253c939f3e486d674d733
FUJIKAKE Takayoshi <fujikake.takayoshi@gmail.com>
2026-07-15 15:51:12 +0900

    lisの長さ３の最小版を更新していなかった

--- 0324_0334/334_IncreasingTripletSubsequence_chatgpt.py
+++ 0324_0334/334_IncreasingTripletSubsequence_chatgpt.py
@@ -1,31 +1,14 @@
 class Solution:
     def increasingTriplet(self, nums: list[int]) -> bool:
-        i, j, k = 0, 0, 0
-        n = len(nums)
-        while i < n - 2:
-            j = i + 1
-            while j < n - 1:
-                # print(i, j)
-                if nums[i] >= nums[j]:
-                    break
-                k = j + 1
-                while k < n:
-                    # print(i,j,k)
-                    if nums[i] < nums[j] < nums[k]:
-                        return True
-                    if nums[j] >= nums[k] and nums[k] < nums[i]:
-                        i = k
-                        j = i + 1
-                        break
-                    if nums[j] >= nums[k] and nums[k] > nums[i]:
-                        j = k
-                        k += 1
-                        continue
-                    k += 1
-                if k == n:
-                    return False
-                j += 1
-            if j == n - 1:
-                return False
-            i += 1
+        first = float("inf")
+        second = float("inf")
+
+        for x in nums:
+            if x <= first:
+                first = x
+            elif x <= second:
+                second = x
+            else:
+                return True
+
         return False
