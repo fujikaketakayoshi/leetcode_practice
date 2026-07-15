7c53729b8251c368d84d4681d76baafdca720770
FUJIKAKE Takayoshi <fujikake.takayoshi@gmail.com>
2026-07-15 15:06:29 +0900

    長さ3のLISが存在するかを求める問題。firstとsecondをINFで変数を用意して、first以下の値が来たらfirstを更新し、firstを超えてsecond以下の値が来たらsecondを更新。その後first,secondを超える値が来たらTrueを返すという実装

---
+++ 334_IncreasingTripletSubsequence_chatgpt.py
@@ -0,0 +1,31 @@
+class Solution:
+    def increasingTriplet(self, nums: List[int]) -> bool:
+        i, j, k = 0, 0, 0
+        n = len(nums)
+        while i < n - 2:
+            j = i + 1
+            while j < n - 1:
+                # print(i, j)
+                if nums[i] >= nums[j]:
+                    break
+                k = j + 1
+                while k < n:
+                    # print(i,j,k)
+                    if nums[i] < nums[j] < nums[k]:
+                        return True
+                    if nums[j] >= nums[k] and nums[k] < nums[i]:
+                        i = k
+                        j = i + 1
+                        break
+                    if nums[j] >= nums[k] and nums[k] > nums[i]:
+                        j = k
+                        k += 1
+                        continue
+                    k += 1
+                if k == n:
+                    return False
+                j += 1
+            if j == n - 1:
+                return False
+            i += 1
+        return False
