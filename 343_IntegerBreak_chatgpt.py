f63dd6d06a1101f2bbc027dd7216330f83026a06
FUJIKAKE Takayoshi <fujikake.takayoshi@gmail.com>
2026-08-04 19:30:00 +0900

    ChatGPTのDFSコードを正しくコピペできていなかった

--- 343_IntegerBreak_chatgpt.py
+++ 343_IntegerBreak_chatgpt.py
@@ -1,33 +1,37 @@
 from functools import cache
+
 class Solution:
     def integerBreak(self, n: int) -> int:
-        nnums = [1] * n
         ans = 0
 
         @cache
         def dfs(nums):
-            nums = list(nums)
-            print(nums)
             nonlocal ans
-            
+
+            nums = list(nums)
             ln = len(nums)
+
             if ln >= 2:
-                p = 1
+                product = 1
                 for num in nums:
-                    p *= num
-                ans = max(ans, p)
+                    product *= num
+                ans = max(ans, product)
+
             if ln == 2:
                 return
-            
-            for i in range(ln):
-                new_nums = nums[:]
-                numi = new_nums.pop(i)
-                newi_nums = new_nums[:]
-                for j in range(i + 1, ln - 1):
-                    numj = newi_nums.pop(j)
-                    newi_nums.append(numi + numj)
-                    dfs(tuple(newi_nums))
-                    newi_nums = new_nums[:]
 
-        dfs(tuple(nnums))
+            for i in range(ln):
+                for j in range(i + 1, ln):
+                    new_nums = []
+
+                    for k in range(ln):
+                        if k != i and k != j:
+                            new_nums.append(nums[k])
+
+                    new_nums.append(nums[i] + nums[j])
+                    new_nums.sort()
+
+                    dfs(tuple(new_nums))
+
+        dfs(tuple([1] * n))
         return ans
