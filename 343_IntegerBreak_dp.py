74886896f6b03df4e4eeae46d8ad26629281e0c5
FUJIKAKE Takayoshi <fujikake.takayoshi@gmail.com>
2026-08-04 19:06:59 +0900

    DFSだとn<=58の制約だがn=40でTLE。正しくはDPを用いて、nをj,i-jと分割して考える。iは2からnまでのループ、jは1からi-1までのループにすることで、すべての分割パターンを網羅でき、遷移式で、j*(i-j),j*dp[i-j]を比較することで今のjとi-jの２分割の掛け算がここまでの最大値となる場合とdp[i-j]でその分割をして過去に求まっている最大値を採用した場合とで考える。dp[i]はそれまでのiとjで求まったdp[i]の最大値が既に存在する場合はそっちを採用するということ

---
+++ 343_IntegerBreak_dp.py
@@ -0,0 +1,18 @@
+class Solution:
+    def integerBreak(self, n: int) -> int:
+        dp = [0] * (n + 1)
+
+        for i in range(2, n + 1):
+            for j in range(1, i):
+                print(i, j, dp[i], j * (i - j), j * dp[i - j])
+                dp[i] = max(
+                    dp[i],
+                    j * (i - j),
+                    j * dp[i - j]
+                )
+
+        print(dp)
+        return dp[n]
+
+s = Solution()
+print(s.integerBreak(10))
