e1ce949ff973c8bbdaa349732390f15d77be77f0
FUJIKAKE Takayoshi <fujikake.takayoshi@gmail.com>
2026-09-03 17:31:13 +0900

    wを昇順、hを降順でソートして、hを配列に取り出し、LISの長さを求めると答え

---
+++ 354_RussianDollEnvelopes_chatgpt.py
@@ -0,0 +1,15 @@
+from bisect import bisect_left
+class Solution:
+    def maxEnvelopes(self, envelopes: List[List[int]]) -> int:
+        envelopes.sort(key=lambda x: (x[0], -x[1]))
+        print(envelopes)
+        heights = [h for w, h in envelopes]
+        lis = []
+        for x in heights:
+            i = bisect_left(lis, x)
+
+            if i == len(lis):
+                lis.append(x)
+            else:
+                lis[i] = x
+        return len(lis)
