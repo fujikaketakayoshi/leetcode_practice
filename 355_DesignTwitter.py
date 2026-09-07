c7b785e73f0c83c26f24a5251fd6d0d8fe6a64de
FUJIKAKE Takayoshi <fujikake.takayoshi@gmail.com>
2026-09-07 17:35:25 +0900

    愚直にtweetsをreverseした配列からuserIdがfollowerに含まれている場合にfeedsにtweetIdを10件まで入れていくとするとTLEする。私の次実装は、userごとにtweetsをtweetIdとtweetPosを保存して、followerのuserのみのtweetsをマージした配列に対してtweetPosで降順ソートしたものの末尾から10件分のtweetIdをfeedsに入れてACでした。ChatGPTの模範解答はk-way mergeという手法でheapに各ユーザーの最新のtweetPosで並べて最新のものを採用してそのtweetIdのユーザーの次に新しいtweetの情報をheapに再度入れてを繰り返して10件取り出すというものでした。

---
+++ 355_DesignTwitter.py
@@ -0,0 +1,40 @@
+class Twitter:
+    def __init__(self):
+        MAX = 500
+        self.user_follows = [set() for _ in range(MAX + 1)]
+        self.user_tweets = [[] for _ in range(MAX + 1)]
+        self.tweet_pos = 0
+
+    def postTweet(self, userId: int, tweetId: int) -> None:
+        if not userId in self.user_follows[userId]:
+            self.user_follows[userId].add(userId)
+        self.tweet_pos += 1
+        self.user_tweets[userId].append((self.tweet_pos, tweetId))
+        
+    def getNewsFeed(self, userId: int) -> List[int]:
+        users = self.user_follows[userId]
+        feeds = []
+        for u in users:
+            feeds += self.user_tweets[u]
+        feeds.sort(reverse=True)
+        ans = []
+        for pos, tweetId in feeds[:10]:
+            ans.append(tweetId)
+        return ans
+
+    def follow(self, followerId: int, followeeId: int) -> None:
+        if not followeeId in self.user_follows[followerId]:
+            self.user_follows[followerId].add(followeeId)
+
+    def unfollow(self, followerId: int, followeeId: int) -> None:
+        if followeeId in self.user_follows[followerId]:
+            self.user_follows[followerId].remove(followeeId)
+        
+
+
+# Your Twitter object will be instantiated and called as such:
+# obj = Twitter()
+# obj.postTweet(userId,tweetId)
+# param_2 = obj.getNewsFeed(userId)
+# obj.follow(followerId,followeeId)
+# obj.unfollow(followerId,followeeId)
---
+++ 355_DesignTwitter_chatgpt.py
@@ -0,0 +1,49 @@
+import heapq
+class Twitter:
+    def __init__(self):
+        MAX = 500
+        self.user_follows = [set() for _ in range(MAX + 1)]
+        self.user_tweets = [[] for _ in range(MAX + 1)]
+        self.tweet_pos = 0
+
+    def postTweet(self, userId: int, tweetId: int) -> None:
+        if not userId in self.user_follows[userId]:
+            self.user_follows[userId].add(userId)
+        self.tweet_pos += 1
+        self.user_tweets[userId].append((tweetId, self.tweet_pos))
+        
+    def getNewsFeed(self, userId: int) -> List[int]:
+        users = self.user_follows[userId]
+        hq = []
+        for userId in users:
+            if self.user_tweets[userId]:
+                tId, tPos = self.user_tweets[userId][-1]
+                heapq.heappush(hq, (-tPos, tId, userId, len(self.user_tweets[userId]) - 1))
+        
+        feeds = []
+        while len(feeds) < 10 and len(hq) > 0:
+            # print(hq)
+            tPos, tId, userId, idx = heapq.heappop(hq)
+            # self.user_tweets[userId].pop()
+            feeds.append(tId)
+            if idx > 0 and self.user_tweets[userId]:
+                tId, tPos = self.user_tweets[userId][idx - 1]
+                heapq.heappush(hq, (-tPos, tId, userId, idx - 1))
+        return feeds
+
+    def follow(self, followerId: int, followeeId: int) -> None:
+        if not followeeId in self.user_follows[followerId]:
+            self.user_follows[followerId].add(followeeId)
+
+    def unfollow(self, followerId: int, followeeId: int) -> None:
+        if followeeId in self.user_follows[followerId]:
+            self.user_follows[followerId].remove(followeeId)
+        
+
+
+# Your Twitter object will be instantiated and called as such:
+# obj = Twitter()
+# obj.postTweet(userId,tweetId)
+# param_2 = obj.getNewsFeed(userId)
+# obj.follow(followerId,followeeId)
+# obj.unfollow(followerId,followeeId)
